b2823fc097e1132223f83034ad192897d09facc4
sunkishg <sunkishg@users.noreply.github.com>
2018-03-17 20:56:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def get_timestamp(day_name):
         deltatotarget = 7
     lastfriday = today - datetime.timedelta(days=deltatotarget)
     myday = lastfriday.strftime("%Y,%m,%d,%w,%j")
-    print myday
+    print(myday)
     myday = myday.split(",")
     myday.append(time.localtime(time.time())[-1])
     am_time_tuple = (int(myday[0]), int(myday[1]), int(myday[2]), 5, 26, 0, int(int(myday[3]) - 1), int(myday[4]), int(myday[-1]))
